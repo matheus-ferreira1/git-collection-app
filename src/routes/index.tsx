360bba2f4b798ad87257791b4bb4158b1b5e7737
Matheus Ferreira <matheus.fer33@gmail.com>
2022-10-31 20:42:36 -0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "../pages/Dashboard";
+import { Repo } from "../pages/Repo";
+
+export const MyRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/repositories" element={<Repo />} />
+    </Routes>
+  );
+};
